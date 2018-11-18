79dbbf400cd6697246a1d216417b9f1dae7397ee
Miheel <mikaelleuf@gmail.com>
2018-11-18 20:12:24 +0100

    Conditional arguments for input and flipped cards

--- Memory/memory.py
+++ Memory/memory.py
@@ -43,44 +43,55 @@ def main():
     main loop
     """
     play_loop = True
-    atempts = 0
     player_input = 0
-    taken= [] #* 20
+    tries = 0
+    taken = [] #* 20
     matrix_num = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
     for i in range(0, 20):
-        taken.append(0)
+        taken.append(0) #Loads th lost with 0
+
     shuffle(matrix_num)
 
     print("Welcome to memory")
     print("Chose two coordinates in the matrix (x y)")
+    print_matrix(matrix_num, taken)
+
     while play_loop:
 
-        while player_input < 2:
+        while player_input < 2: #Input loop 
+
             x_input = int(input("input1 X:"))
             y_input = int(input("input2 Y:"))
 
-            if player_input == 0:
-                card_1 = y_input * 5 + x_input
-            elif player_input == 1:
-                card_2 = y_input * 5 + x_input
-            player_input += 1
+            if 0 <= x_input < 5 and 0 <= y_input < 4: #Check if input is inside list interval
+                if player_input == 0:
+                    card_1 = y_input * 5 + x_input
+
+                elif player_input == 1:
+                    card_2 = y_input * 5 + x_input
+                tries += 1
+                player_input += 1
+            else:
+                print("invalid input")
 
         print(matrix_num[card_1])
         print(matrix_num[card_2])
-        print_matrix(matrix_num, taken)
 
-        taken[card_1] = 1
-        taken[card_2] = 1
-        print_matrix(matrix_num, taken)
+        if matrix_num[card_1] == matrix_num[card_2]: #Determines if the cards are equal
+            taken[card_1] = 1
+            taken[card_2] = 1
+            print_matrix(matrix_num, taken)
+        else:
+            print("the cards were not equal try again")
+
         print(taken)
-        if 0 in taken:
+
+        if 0 in taken: #Determines if the board has bee filed
             player_input = 0
         else:
             print("cleared board")
+            print("It took you: ", tries)
             play_loop = False
-
-
 
 if __name__ == "__main__":
     main()
-
