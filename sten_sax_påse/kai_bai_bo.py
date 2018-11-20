c44f42a30668dd2f32a55932031d3e976a109549
Miheel <mikaelleuf@gmail.com>
2018-11-20 16:09:14 +0100

    Fixed comment of code

--- sten_sax_påse/kai_bai_bo.py
+++ sten_sax_påse/kai_bai_bo.py
@@ -18,33 +18,34 @@ def main():
     player_points = 0
     pc_points = 0
     play_loop = True
-	
+
     print("Welcome to the Game ROCK-PAPER-SCISSORS!\n")
     name = input("Please type your name ")
     print("Welcome ", name, " What's your choise\n")
 
     while play_loop:
-        print("ROCK(1), PAPER(2), SCISSORS(3), or end game(0)")
-        player_input = input("input: ")
+        print("ROCK(1), PAPER(2), SCISSORS(3), or end game(q or quit)")
+        player_input = input("input: ") #input to str
 
-        pc_choice = str(rand())
+        pc_choice = str(rand()) #convert value from rand int to str
 
         if player_input in ("1", "2", "3"):
 
-            if player_input == pc_choice:
-                print("it'a draw\n")
+            if player_input == pc_choice: #Argument for a draw
+                print("it'a draw\npc: ", moves[int(pc_choice)-1])
+                print(name, ":", moves[int(player_input)-1], "\n")
 
-            elif player_input == "1" and pc_choice == "3" or player_input == "2" and pc_choice == "1" or player_input == "3" and pc_choice == "2":
-                print("congrats you win\npc: ", moves[int(pc_choice)-1])
+            elif player_input == "1" and pc_choice == "3" or player_input == "2" and pc_choice == "1" or player_input == "3" and pc_choice == "2": #Argument for a player wim
+                print("congrats you win\npc: ", moves[int(pc_choice)-1]) #takes value from list moves to write out move instead number
                 print(name, ":", moves[int(player_input)-1], "\n")
                 player_points += 1
 
-            else:
+            else: #Argument for a player win
                 print("you loose\npc: ", moves[int(pc_choice)-1])
                 print(name, ":", moves[int(player_input)-1], "\n")
                 pc_points += 1
 
-        elif player_input in ("q", "quit"):
+        elif player_input in ("q", "quit"): #Argument to end th game
             print(name, "have", player_points, "points.\nPc have ", pc_points, "points.")
             print("Exiting game")
             input("Press enter exit")
