0018c2023d2e9ef0ffe7f5925e7a1c3c0a9a15a3
Miheel <mikaelleuf@gmail.com>
2018-12-12 21:18:39 +0100

    cleand up code and added alternating colors

--- Lab3/sierpinskis_tiangel/sierpinskis_triangel.py
+++ Lab3/sierpinskis_tiangel/sierpinskis_triangel.py
@@ -22,7 +22,17 @@ def sierpinski(win, size, start, color):
 
     triangle = Polygon(p1, p2, p3)
     triangle.setFill(color)
+    
+    if color == "white":
+        color = "red"
+        triangle.setFill(color)
+    
+    elif color == "red":
+        color = "white"
+        triangle.setFill(color)
     triangle.draw(win)
+    
+    start_p_tri = start
 
     #Sets the new startin point the rightmost triangel 
     #By adding half the side length to the X value
@@ -33,24 +43,12 @@ def sierpinski(win, size, start, color):
     #and one half the height of the prior triangle to the Y value
     point_d_tri = Point(start.getX() + (size / 4), start.getY() + (height / 2)) 
 
-    if size > 5:
-        #count = 
-        #if count % 2 == 0:
-        #    triangle.undraw()
-        #else:
-        #    triangle.draw
-        #sierpinski(win, size / 2, start, color)
-        #sierpinski(win, size / 2, point_r_tri, color)
-        #sierpinski(win, size / 2, point_d_tri, color)
+    if size > 10:
+        sierpinski(win, size / 2, start_p_tri, color)
         
-        #triangle.draw(win)
-
-        tri_l = sierpinski(win, size / 2, start, color)
-        tri_r = sierpinski(win, size / 2, point_r_tri, color)
-        tri_d = sierpinski(win, size / 2, point_d_tri, color)
-
-        tri =[tri_l, tri_r, tri_d]
-        return tri
+        sierpinski(win, size / 2, point_r_tri, color)
+        
+        sierpinski(win, size / 2, point_d_tri, color)
 
     if size <= 5:
         return 0
@@ -59,13 +57,14 @@ def main():
     win = GraphWin("Triangel", 1600, 900)
     win.setBackground("green")
     side_len = 800
-
+    col_tri = "red"
+    
     #Calculates the triangels stating pos
     win_w = (win.getWidth() / 2) - (side_len / 2)
     win_h = (win.getHeight() / 2) - (side_len / 2)
 
     start_p = Point(win_w, win_h)
-    col_tri = "red"
+
     sierpinski(win, side_len, start_p, col_tri)
     win.getMouse()
 
